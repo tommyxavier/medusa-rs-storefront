63b7b40302c5a061a0aecb958148bbf2144510d0
tommyxavier <tommyxavierv@hotmail.com>
2025-07-18 17:05:07 -0400

    next.config,js

--- next.config.js
+++ next.config.js
@@ -20,9 +20,20 @@ const nextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: "http",
+      // Remove this if you're not using localhost for images at all
+       {
+         protocol: "http",
         hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "iajnjwbxmuvbvpykkorq.supabase.co", // <--- ADD THIS LINE FOR YOUR SUPABASE S3 BUCKET HOSTNAME
+      },
+      // Keep other S3 domains if you anticipate images from those sources too,
+      // but if all your current images are on Supabase, these might be redundant.
+      {
+        protocol: "https",
+        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
       },
       {
         protocol: "https",
